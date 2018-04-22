798761a8f1b41163722d712982a60d58f7585e1e
Dr-James <drjames@bsu.edu>
2018-04-22 13:31:06 -0400

    Edited create_pictures() to use the index of the count of Picture, rather than a local counter

--- polls/createPictures.py
+++ polls/createPictures.py
@@ -6,9 +6,7 @@ import datetime
 
 def create_pictures():
     os.chdir('/Users/DJames9238/PycharmProjects/deepwatchdog/media/')
-    counter = 0
     for file in glob.glob("*.png"):
-        picture = Picture(counter, 1, datetime.datetime.now().strftime('%m-%d-%Y %H:%M:%S'), "/media/" + file)
+        picture = Picture(Picture.objects.all().count(), 1, datetime.datetime.now().strftime('%m-%d-%Y %H:%M:%S'), "/media/" + file)
         picture.save()
-        counter += 1
     os.chdir('/Users/DJames9238/PycharmProjects/deepwatchdog/')
